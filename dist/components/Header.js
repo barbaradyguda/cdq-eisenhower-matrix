73a20e90265c6170011d75c635f1efd3433f3ccc
barbaradyguda <bdyguda@gmail.com>
2022-06-19 22:26:48 +0200

    cleaning

--- dist/components/Header.js
+++
@@ -1,5 +0,0 @@
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-var CustomButton = styled(Button)({
-// your custom styles go here
-});
--- dist/components/matrix/matrixSquare.js
+++
@@ -1,7 +0,0 @@
-import { jsxs as _jsxs } from "react/jsx-runtime";
-import "../../App.css";
-var MatrixSquare = function (_a) {
-    var urgentImportantTasks = _a.urgentImportantTasks, urgentTasks = _a.urgentTasks, importantTasks = _a.importantTasks, otherTasks = _a.otherTasks;
-    return (_jsxs("div", { children: [urgentImportantTasks && urgentImportantTasks.map(function (urgentImportantTask) { return (_jsxs("p", { children: ["UI ", urgentImportantTask.title] })); }), urgentTasks && urgentTasks.map(function (urgentTask) { return (_jsxs("p", { children: ["U ", urgentTask.title] })); }), importantTasks && importantTasks.map(function (importantTask) { return (_jsxs("p", { children: ["I", importantTask.title] })); }), otherTasks && otherTasks.map(function (otherTask) { return (_jsxs("p", { children: ["O ", otherTask.title] })); })] }));
-};
-export default MatrixSquare;
